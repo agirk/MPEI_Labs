ce84e9f3fa9646e7471885dd863322835d5500c4
Лапина Анастасия <55759767+agirk@users.noreply.github.com>
2023-10-03 12:22:06 +0300

    Лаб1

--- ВС/BC1MPI/BC1MPI/BC1MPI.cpp
+++ ВС/BC1MPI/BC1MPI/BC1MPI.cpp
@@ -2,71 +2,73 @@
 #include <vector>
 #include <time.h>
 #include <vector>
-#include <chrono>
 #include "mpi.h"
 using namespace std;
 
 void consecutive(const int N) {
-    const double S1 = 1.0;
+    const double S1 = 2.5;
     vector <double> A, B, C, Y;
     A.resize(N); B.resize(N); C.resize(N); Y.resize(N);
 
-    chrono::high_resolution_clock::time_point timeStart = chrono::high_resolution_clock::now();
     for (int i = 0; i < N; i++) {
         A[i] = i;
         B[i] = i * 2;
         C[i] = (i + 1) / 2;
         Y[i] = (A[i] + B[i] * S1 + C[i]) * A[i];
     }
-    chrono::high_resolution_clock::time_point timeEnd = chrono::high_resolution_clock::now();
-    chrono::duration<double, milli> milli_diff = timeEnd - timeStart;
 
-    cout << "  Time (ms): " << milli_diff.count() << endl << endl << "  Result:" << endl;
     for (int i = 0; i < N; i++) 
-        cout << "  " << Y[i] << endl;
-   
+        cout << "  " << Y[i] << endl; 
 }
 
 int main(int argc, char** argv) {
-    const int N = 12;
-    
     int ProcRank, size;
     vector <double> A, B, C, Y;
-    A.resize(N); B.resize(N); C.resize(N); Y.resize(N);
-    const double S1 = 1.0;
+    vector <int> N_val = { 8, 64, 256, 1024, 4096 };
+    const double S1 = 2.5;
+    double timeStart, timeEnd;
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    if (ProcRank == 0) {
 
-        for (int i = 0; i < N / size; i++) {
-            cout << "rank: " << ProcRank << "  i: " << i << endl;
-            A[i] = i;
-            B[i] = i * 2;
-            C[i] = (i + 1) / 2;
-            Y[i] = (A[i] + B[i] * S1 + C[i]) * A[i];
+    for (int k = 0; k < N_val.size(); k++) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        A.resize(N_val[k]); B.resize(N_val[k]); C.resize(N_val[k]); Y.resize(N_val[k]);
+
+        if (ProcRank == 0) {
+            timeStart = MPI_Wtime();
+            for (int i = 0; i < N_val[k] / size; i++) {
+                //cout << "rank: " << ProcRank << "  i: " << i << endl;
+                A[i] = i;
+                B[i] = i * 2;
+                C[i] = (i + 1) / 2;
+                Y[i] = (A[i] + B[i] * S1 + C[i]) * A[i];
+            }
+
+            for (int i = 1; i < size; i++) { // Сбор результатов 
+                MPI_Recv(&Y[(int)i * N_val[k] / size], (int)N_val[k] / size, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+            }
+
+            timeEnd = MPI_Wtime();
+
+            cout << "Time: " << timeEnd - timeStart << endl << endl;// << "  Result:" << endl;
+            //  for (int i = 0; i < N; i++)
+            //      cout << "  " << Y[i] << endl;
+
+              //cout << endl << endl <<  "  Consecutive programm" << endl;
+              //consecutive(N); // Последовательная программа
         }
-
-        for (int i = 1; i < size; i++) // Сбор результатов 
-            MPI_Recv(&Y[(int)i * N / size], (int)N / size, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
-       
-        cout << endl << endl;
-        for (int i = 0; i < N; i++)
-            cout << "  " << Y[i] << endl;
-
-        cout << endl << "  Consecutive programm" << endl;
-        consecutive(N); // Последовательная программа
-    }
-    else {
-        for (int i = ProcRank * N / size; i < (ProcRank + 1) * N / size; i++) {
-            cout << "rank: " << ProcRank << "  i: " << i << endl;
-            A[i] = i;
-            B[i] = i * 2;
-            C[i] = (i + 1) / 2;
-            Y[i] = (A[i] + B[i] * S1 + C[i]) * A[i];
+        else {
+            for (int i = ProcRank * N_val[k] / size; i < (ProcRank + 1) * N_val[k] / size; i++) {
+                //cout << "rank: " << ProcRank << "  i: " << i;
+                A[i] = i;
+                B[i] = i * 2;
+                C[i] = (i + 1) / 2;
+                Y[i] = (A[i] + B[i] * S1 + C[i]) * A[i];
+            }
+            MPI_Send(&Y[(int)ProcRank * N_val[k] / size], (int)N_val[k] / size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         }
-        MPI_Send(&Y[(int)ProcRank * N / size], (int)N / size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
